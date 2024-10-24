7105846319cbc0ebdb71909b885afecef2619285
daphnet9 <daphnet9@nycstudents.net>
2024-10-24 15:50:21 +0000

    isWordChain method

--- WordChecker.java
+++ WordChecker.java
@@ -1,1 +1,22 @@
+import java.util.ArrayList;
 
+public class WordChecker
+{
+    public ArrayList<String> wordList; 
+
+    public WordChecker(ArrayList<String> list)
+    {
+        wordList = new ArrayList<String>();
+    }
+
+    public boolean isWordChain()
+    {
+        for(int i = 1; i < wordList.size(); i++)
+        {
+            String after = wordList.get(i);
+            String before = wordList.get(i - 1);
+            if(after.indexOf(before) < 0) return false;
+        }
+        return(true);
+    }
+}
